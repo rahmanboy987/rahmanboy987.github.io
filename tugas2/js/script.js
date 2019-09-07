b3f882da5aad61f2812830eb04dc2ded7c58eac1
Moh. Fathur Rohman <rahmanboy987@gmail.com>
2019-09-07 08:05:13 +0700

    ganti

---
+++ tugas2/js/script.js
@@ -0,0 +1,8 @@
+$('.page-scroll').on('click', function (e)) {
+    var tujuan = $(this).attr('href');
+    var elementTujuan = (tujuan);
+
+    $('body').animate({
+        scrollTop: elementTujuan.offset().top - 50
+    });
+}
