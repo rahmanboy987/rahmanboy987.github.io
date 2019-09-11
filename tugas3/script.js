7b7cd8434061c1e21616dc49320fdf9db9477c2a
Moh. Fathur Rohman <rahmanboy987@gmail.com>
2019-09-11 15:21:47 +0700

    update

---
+++ tugas3/script.js
@@ -0,0 +1,47 @@
+function no1() {
+    document.getElementById('attribute1').style.display = 'none';
+}
+
+function yes1() {
+    document.getElementById('attribute1').style.display = 'block';
+}
+
+function no2() {
+    document.getElementById('attribute2').style.display = 'none';
+}
+
+function yes2() {
+    document.getElementById('attribute2').style.display = 'block';
+}
+
+function no3() {
+    document.getElementById('attribute3').style.display = 'none';
+}
+
+function yes3() {
+    document.getElementById('attribute3').style.display = 'block';
+}
+
+function no4() {
+    document.getElementById('attribute4').style.display = 'none';
+}
+
+function yes4() {
+    document.getElementById('attribute4').style.display = 'block';
+}
+
+function no5() {
+    document.getElementById('attribute5').style.display = 'none';
+}
+
+function yes5() {
+    document.getElementById('attribute5').style.display = 'block';
+}
+
+function no6() {
+    document.getElementById('attribute6').style.display = 'none';
+}
+
+function yes6() {
+    document.getElementById('attribute6').style.display = 'block';
+}
